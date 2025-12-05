429a369c1b7fc050c8625b41e482b203edc25411
MODI SACHIN ASHVINBHAI <159873878+sachin001752@users.noreply.github.com>
2025-12-05 11:53:41 +0530

    Add files via upload

---
+++ img/sachin.js/montu.js
@@ -0,0 +1,55 @@
+/*let mode= "red";
+let color;
+
+if(mode==="dark"){
+    color="black";
+}else if(mode==="blue"){
+    color="blue";
+}else if(mode==="pink"){
+    color="pink";
+}else{
+    color="white";
+}
+
+console.log(color);*/ 
+
+/*let age = 30;
+sachin=age>=18 ? "adult" :"not adult";
+
+console.log(sachin); */
+
+
+/*let  name =prompt("hello!");
+console.log(name);*/
+
+//for(let i=1; i<=10;i++)
+//{
+   // console.log("modi sachin montu");
+    //console.log("modi");
+//}
+
+//calculate sum of 1 to 5
+/*let sum = 0;
+for (let i =1;i<=10;i++)
+{
+    sum=sum + i;
+}
+console.log("i=",sum)
+console.log("loop has ended");*/
+
+
+/*let i=1;
+while(i<=10)
+{
+    console.log("i=",i);
+    i++;
+}*/
+
+/*let str ="modisachin";
+ for(let i of str)
+ {
+    console.log("i=",i);
+ }*/
+
+  
+
